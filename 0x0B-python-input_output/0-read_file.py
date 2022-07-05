2579d0c81f24c3b81f8cb15d55de76618aaee614
MercyMukiri <mwmukiri@gmail.com>
2022-07-05 12:27:43 -0700

    Add files README.md and 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module that reads a file and prints its contents"""
+
+
+def read_file(filename=""):
+    """ This function reads the contents of a file
+    Args:
+         filename (string): name of the file
+    Raises:
+         Exception: when it fails to open a file
+    """
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end='')
