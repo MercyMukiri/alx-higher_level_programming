7f8567a91ff4e37c37b0d17a6dcb49974eaf3559
MercyMukiri <mwmukiri@gmail.com>
2022-07-05 12:35:42 -0700

    Add file 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module that has a function which writes a string file """
+
+
+def write_file(filename="", text=""):
+    """ This function writes text to a file
+    Args:
+        filename (string): Name of input file
+        text (string): Output text
+    Raises:
+        Exception: When file fails to open
+    """
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        return (f.write(text))
