f7c7fb0e1508e8f3a96bc29699b5f8607bb6ad76
MercyMukiri <mwmukiri@gmail.com>
2022-08-10 09:35:10 -0700

    Add file 102-rating_shows.sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- A script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+
+SELECT title, SUM(rate) AS rate
+       FROM tv_shows AS shows
+       	    INNER JOIN tv_show_ratings AS ratings
+	    ON shows.id = ratings.show_id
+	GROUP BY title
+ORDER BY rate DESC;
