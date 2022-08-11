e797d25e45388ddc61fc2ac3b4d388adc18780af
MercyMukiri <mwmukiri@gmail.com>
2022-08-10 19:17:10 -0700

    Update file 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,8 @@
 -- A script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 
-SELECT title, SUM(ratings.rate) AS rate
+SELECT title, SUM(ratings.rate) AS rating
        FROM tv_shows AS shows
        	    INNER JOIN tv_show_ratings AS ratings
 	    ON shows.id = ratings.show_id
 	GROUP BY title
-ORDER BY rate DESC;
+ORDER BY rating DESC;
