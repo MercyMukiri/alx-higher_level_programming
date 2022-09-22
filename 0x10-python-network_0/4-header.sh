4da02fb2411f59979d9784a6eca79c59d8d833d0
MercyMukiri <mwmukiri@gmail.com>
2022-09-22 15:23:17 -0700

    Update file 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET request with a header variable X-School-User-Id whose value is 98 to the URL, and displays the body of the response
-curl -sX GET -H "X-School-User-Id: 98" "$1"
+# script that sends a GET request to URL and displays the body of the response
+curl -sH "X-School-User-Id: 98" -X GET "$1"
