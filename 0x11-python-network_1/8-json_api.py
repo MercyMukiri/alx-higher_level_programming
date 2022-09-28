680ba8029ab304da230d0d9561bf58d4409da6a8
MercyMukiri <mwmukiri@gmail.com>
2022-09-27 18:24:37 -0700

    Update file 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """Takes in a letter and sends a POST request to URL"""
 
+import requests
+import sys
 
 if __name__ == "__main__":
-    import requests
-    import sys
-
-    query = ""
     if len(sys.argv) > 1:
         query = sys.argv[1]
-        res = requests.post('http://0.0.0.0:5000/search_user', data={'q': query})
-        try:
-            result = res.json()
-            if result == {}:
-                print('No result')
-            else:
-                print("[{}] {}".format(result.get('id'), result.get('name')))
-        except ValueError:
-            print('Not a valid JSON')
+    else:
+        query = ""
+
+    res = requests.post('http://0.0.0.0:5000/search_user', data={'q': query})
+
+    try:
+        result = res.json()
+        if result == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(result.get('id'), result.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
