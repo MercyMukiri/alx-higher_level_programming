b0d2d52d51da8bce462f55b75ffa3ce99e23aa25
Mercy Mukiri <mwmukiri@gmail.com>
2022-10-18 18:38:11 -0700

    Make 0-readme.js semistandard compliant

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', (err, result) => {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log(result);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+  }
 });
